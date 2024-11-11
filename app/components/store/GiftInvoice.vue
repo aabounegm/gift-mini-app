bb1faa4d61c305e815dd57b3da1e431976f46db1
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-11-11 11:25:28 +0200

    Replace balloons image with transparent bg one

--- app/components/store/GiftInvoice.vue
+++ app/components/store/GiftInvoice.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import type { Gift } from "~~/shared/types";
-import ballonsImg from "~/assets/images/ballons.png";
+import balloonsImg from "~/assets/images/ballons.png";
 
 defineProps<{ gift: Gift }>();
 </script>
 
 <template>
   <div class="flex flex-col gap-2 justify-center h-full text-center py-4">
-    <img :src="ballonsImg" class="size-32 mx-auto rounded-full" />
+    <img :src="balloonsImg" class="balloon size-32 p-4 mx-auto rounded-full" />
     <h1 class="text-lg">Invoice from Gift App</h1>
     <p class="font-bold text-3xl">{{ gift.price }} {{ gift.currency }}</p>
     <p class="text-label-secondary">${{ gift.price }}</p>
     <p>Purchasing a {{ gift.name }} gift</p>
   </div>
 </template>
+
+<style scoped>
+.balloon {
+  background: linear-gradient(180deg, #a1d1f2 0%, #a898f4 100%);
+}
+</style>
