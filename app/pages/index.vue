4fb97f93304ba680832856e96d7c4e97d6f7744e
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-11-11 18:36:54 +0200

    Replace middleware with redirect

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,20 +2,20 @@
 import { useWebApp } from "vue-tg";
 
 definePageMeta({
-  middleware(from) {
+  redirect() {
     const { initDataUnsafe } = useWebApp();
 
-    if (from.path === "/" && initDataUnsafe.start_param) {
+    if (initDataUnsafe.start_param) {
       const params = new URLSearchParams(
         decodeURIComponent(initDataUnsafe.start_param)
       );
       const giftId = params.get("gift");
       const fromUserId = params.get("from");
 
-      return navigateTo(`/gifts/${giftId}/received?from=${fromUserId}`);
+      return `/gifts/${giftId}/received?from=${fromUserId}`;
     }
 
-    return navigateTo("/store");
+    return "/store";
   },
 });
 </script>
