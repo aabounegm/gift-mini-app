6ec76252225b72f2b8b4a917b231f425d1aae104
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-11-12 21:36:38 +0200

    Fix gift card colors

--- app/components/profile/GiftCard.vue
+++ app/components/profile/GiftCard.vue
@@ -8,13 +8,13 @@ defineProps<{
 </script>
 
 <template>
-  <div class="bg-gray-100 p-3 rounded-xl relative">
+  <div class="bg-secondary_bg_color p-3 rounded-xl relative">
     <img
       :src="item.sender.profilePicture"
       alt="User"
       class="size-4 rounded-full absolute top-2 left-2"
     />
-    <span class="absolute top-2 right-2 text-xs text-gray-500">
+    <span class="absolute top-2 right-2 text-xs text-hint_color">
       {{ $t("profile.count", { total: compactNumber(item.gift.totalSupply) }) }}
     </span>
     <div class="flex flex-col items-center justify-center mt-5">
